aab8dc285ee0e6d5a808b9fa1a230b269a08afbf
Saman Azadi <s.azadi@DESKTOP-VJHOBTS>
2022-01-15 15:53:52 +0330

    Add Blowfish Cryptography

---
+++ Src/Cryptography_Blowfish/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cryptography_Blowfish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "Blowfish Key";
+
+            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Cryptography Key", key));
+
+            string text = "Hello World";
+
+            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Text Befor Encrypt", text));
+
+            Blowfish service = new(key); 
+
+            var resultEncrypt = service.EncryptBytes(text);
+            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Text Encrypted", resultEncrypt));
+
+            var resultDecrypt = service.DecryptBytes(resultEncrypt);
+            Console.WriteLine(String.Format("{0,-20} {1,-10}", "Text Decrypted", resultDecrypt));
+
+            Console.ReadKey();
+        }
+    }
+}
